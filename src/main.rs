098b3ced358f95f2e284f31398eb9e57e585af78
Galin Kostov <dev@galin.cc>
2023-03-28 16:18:12 +0300

    Add API error enum

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,41 @@ extern crate dotenv_codegen;
 #[macro_use]
 extern crate log;
 
-use reqwest::{Client, Error, Response, header::HeaderValue};
+use reqwest::{Client, Response, header::{HeaderValue, CONTENT_TYPE, AUTHORIZATION}};
 use serde_json::json;
 use data_encoding::BASE64;
+use serde::{Deserialize, Serialize};
+use thiserror::Error as ThisError;
+use std::io::{Error, ErrorKind};
+
+#[derive(ThisError, Debug)]
+pub enum ApiError {
+    #[error("SerdeError: {0}")]
+    SerdeError(#[from] serde_json::Error),
+    #[error("RPC Error: {0}")]
+    RPCError(String),
+    #[error("RequestError: {0}")]
+    RequestError(#[from] reqwest::Error),
+}
+impl From<ApiError> for Error {
+    fn from(err: ApiError) -> Self {
+        Error::new(ErrorKind::Other, err)
+    }
+}
 
 const RPC_USERNAME: &str = dotenv!("RPC_USERNAME");
 const RPC_PASSWORD: &str = dotenv!("RPC_PASSWORD");
+const RPC_DOMAIN_AND_PORT: &str = dotenv!("RPC_DOMAIN_AND_PORT");
 
-
-async fn make_rpc_call(method: &str, params: Vec<&str>) -> Result<Response, Error> {
+async fn make_rpc_call(method: &str, params: Vec<&str>) -> Result<Response, reqwest::Error> {
     let client = Client::new();
     let rpc_request = json!({
         "jsonrpc": "2.0",
         "method": method,
         "params": params,
+        "headers": {
+            "Content-Type": "application/json",
+        },
         "id": 1
     });
 
@@ -24,9 +45,10 @@ async fn make_rpc_call(method: &str, params: Vec<&str>) -> Result<Response, Erro
     let encoded_auth_header_value = BASE64.encode(auth_header_value.as_bytes());
     let auth_header = HeaderValue::from_str(&format!("Basic {}", encoded_auth_header_value)).unwrap();
 
-    let response = client.post("http://localhost:4001")
+    let response = client.post(RPC_DOMAIN_AND_PORT)
         .json(&rpc_request)
-        .header("Authorization", auth_header)
+        .header(AUTHORIZATION, auth_header)
+        .header(CONTENT_TYPE, "application/json")
         .send()
         .await?;
 
@@ -38,12 +60,34 @@ async fn run() -> Result<(), Box<dyn std::error::Error>> {
         .await
         .expect("Could not make RPC call");
 
-    // Parse the JSON response
     let result: serde_json::Value = response.json()
         .await
         .expect("Could not parse JSON response");
 
-    // Print the result
+    println!("=========== getaddresses ==========");
+    println!("{}", result);
+
+    let response = make_rpc_call("getaddresses", vec![])
+        .await
+        .expect("Could not make RPC call");
+
+    let result: serde_json::Value = response.json()
+        .await
+        .expect("Could not parse JSON response");
+
+    println!("\n=========== getaddresses ==========");
+    println!("{}", result);
+
+    // Send a message containing the client's public key
+    let response = make_rpc_call("validateaddress", vec!["1YXWvSKFm4XG5yPiFcXGGSvEwH4D8K2nRcXWeA"])
+        .await
+        .expect("Could not make RPC call: ");
+
+    let result: serde_json::Value = response.json()
+        .await
+        .expect("Could not parse JSON response");
+
+    println!("\n=========== validateaddress ==========");
     println!("{}", result);
 
     Ok(())
