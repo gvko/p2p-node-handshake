e15baaafc84d23606c1df8fe8f4f7b9c3b52a740
Galin Kostov <dev@galin.cc>
2023-03-28 14:57:31 +0300

    Use env vars instead of hardcoding the rpc auth creds

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+#[macro_use]
+extern crate dotenv_codegen;
+#[macro_use]
+extern crate log;
+
 use reqwest::{Client, Error, Response, header::HeaderValue};
 use serde_json::json;
 use data_encoding::BASE64;
 
-async fn make_rpc_call(method: &str, params: Vec<&str>, username: &str, password: &str) -> Result<Response, Error> {
+const RPC_USERNAME: &str = dotenv!("RPC_USERNAME");
+const RPC_PASSWORD: &str = dotenv!("RPC_PASSWORD");
+
+
+async fn make_rpc_call(method: &str, params: Vec<&str>) -> Result<Response, Error> {
     let client = Client::new();
     let rpc_request = json!({
         "jsonrpc": "2.0",
@@ -11,7 +20,7 @@ async fn make_rpc_call(method: &str, params: Vec<&str>, username: &str, password
         "id": 1
     });
 
-    let auth_header_value = format!("{}:{}", username, password);
+    let auth_header_value = format!("{}:{}", RPC_USERNAME, RPC_PASSWORD);
     let encoded_auth_header_value = BASE64.encode(auth_header_value.as_bytes());
     let auth_header = HeaderValue::from_str(&format!("Basic {}", encoded_auth_header_value)).unwrap();
 
@@ -25,11 +34,7 @@ async fn make_rpc_call(method: &str, params: Vec<&str>, username: &str, password
 }
 
 async fn run() -> Result<(), Box<dyn std::error::Error>> {
-    // Set the username and password
-    let username = "multichainrpc";
-    let password = "secret";
-
-    let response = make_rpc_call("getinfo", vec![], username, password)
+    let response = make_rpc_call("getinfo", vec![])
         .await
         .expect("Could not make RPC call");
 
@@ -46,6 +51,9 @@ async fn run() -> Result<(), Box<dyn std::error::Error>> {
 
 #[tokio::main]
 async fn main() {
+    dotenv::dotenv().ok();
+    env_logger::init();
+
     if let Err(e) = run().await {
         eprintln!("Error: {}", e);
     }
